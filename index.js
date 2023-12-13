ed5dafa57a6588fe12831c392f2c65d692fb6b00
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-12-13 22:45:05 +0100

    .

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log('czesc')
+const fs = require("node:fs").promises;
+const path = require("node:path");
+
+const contactsPath = path.join(__dirname, "/db/", "contacts.json");
+
+const listContacts = () => {
+  fs.readFile(contactsPath)
+    .then((resp) => {
+      console.log(JSON.parse(resp));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const getContactById = (contactId) => {
+  fs.readFile(contactsPath)
+    .then((resp) => {
+      const contactsList = JSON.parse(resp);
+      const results = contactsList.filter((contact) => {
+        return contact.id === contactId;
+      });
+        console.log(results[0]);
+        
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const removeContact = (contactId) => {
+  fs.readFile(contactsPath)
+    .then((resp) => {
+      const contactsList = JSON.parse(resp);
+      const results = contactsList.filter((contact) => {
+        return contact.id === contactId;
+      });
+        const indexToDelete = contactsList.indexOf(results[0]);
+         contactsList.splice(indexToDelete, 1);
+        console.log(contactsList)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const addContact = (name, email, phone) => {
+    
+}
+
+//  listContacts();
+// //   getContactById('05olLMgyVQdWRwgKfg5J6');
+   removeContact('AeHIrLTr6JkxGE6SN-0Rw');
+
