6038cca7f3c6d7dbdce7f0ba74cb92e96d1dde07
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-12-14 20:16:55 +0100

    .

--- contacts.js
+++ contacts.js
@@ -21,7 +21,11 @@ const getContactById = (contactId) => {
       const results = contactsList.filter((contact) => {
         return contact.id === contactId;
       });
-      console.log(results);
+      if (results[0] === undefined) {
+        console.log('Nie ma takiego kontaktu ;( (sprawdź czy wprowadziłeś poprawne id)')
+      } else {
+        console.log(results[0])
+      }
     })
     .catch((err) => {
       console.log(err);
@@ -38,7 +42,7 @@ const removeContact = (contactId) => {
           console.log("Usunięto...");
           contactsList.splice(indexToDelete, 1);
           fs.writeFile(contactsPath, JSON.stringify(contactsList));
-          console.log(contactsList.length);
+          console.table(contactsList)
         }
       });
     })
@@ -56,12 +60,12 @@ const addContact = (name, email, phone) => {
       phone,
       id: Math.random().toString(36).substring(2,12),
     };
-
     contactsList.push(contactToAdd);
     fs.writeFile(contactsPath, JSON.stringify(contactsList));
+    console.log('Pomyślnie dodano kontakt')
+    console.table(contactsList)
   });
 };
-
 
 module.exports ={
     listContacts,
